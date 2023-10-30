c7b0464c505ae06160097433bf57de0e745c8717
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2023-10-30 17:53:32 +0100

    created a document schema for projects

---
+++ _studio/schemas/documents/projects.js
@@ -0,0 +1,27 @@
+export default {
+	title: 'Projects',
+	name: 'projects',
+	type: 'document',
+	fields: [
+		{
+			title: 'Title',
+			name: 'title',
+			type: 'string'
+		},
+		{
+			title: 'Image',
+			name: 'image',
+			type: 'image'
+		},
+		{
+			title: 'Description',
+			name: 'description',
+			type: 'text'
+		},
+		{
+			title: 'URL',
+			name: 'url',
+			type: 'url'
+		}
+	]
+}
