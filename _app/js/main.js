169fde16a59bc2fe2c11bd6395e587db8c0c750b
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2023-11-10 19:30:03 +0100

    imported projects.js

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,5 +1,7 @@
 import navigation from './modules/navigation.js';
+import projects from './modules/projects.js';
 // import darkMode from './modules/dark-mode.js';
 
 navigation();
+projects();
 // darkMode();
