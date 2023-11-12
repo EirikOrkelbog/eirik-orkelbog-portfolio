832a649beeb9d41f3d9705459d9bd79433ea0b5e
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2023-11-12 19:56:08 +0100

    changed query to get image url

--- _app/js/modules/projects.js
+++ _app/js/modules/projects.js
@@ -2,25 +2,24 @@ export default function projects() {
 
 	const PROJECT_ID = "qe45c197";
 	const DATASET = "production";
-	const QUERY = encodeURIComponent('*[_type == "projects"]');
-
+	const QUERY = encodeURIComponent('*[_type == "projects"]{url, title, _id, description, "imageUrl": image.asset->url}');
 	const URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`;
+
 
 	const project = document.querySelector('.project');
 
 	fetch(URL)
 		.then((res) => res.json())
 		.then(({ result }) => {
-			console.log(result[0]);
 			const projectImage = document.createElement('img');
 			const projectTitle = document.createElement('h4');
 			const projectDescription = document.createElement('p');
 			const projectUrl = document.createElement('a');
-			projectImage.setAttribute('src', result[0].image.asset._ref);
+			projectImage.setAttribute('src', result[0].imageUrl);
 			projectTitle.textContent = result[0].title;
 			projectDescription.textContent = result[0].description;
 			projectUrl.setAttribute('href', result[0].url);
-			projectUrl.textContent = 'Link';
+			projectUrl.textContent = 'See live project';
 			project.appendChild(projectImage);
 			project.appendChild(projectTitle);
 			project.appendChild(projectDescription);
