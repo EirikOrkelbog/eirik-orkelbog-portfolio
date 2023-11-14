44602eef779b7060e8e17f7a217929574b9c1ab5
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2023-11-14 18:48:14 +0100

    looping through results from sanity api

--- _app/js/modules/projects.js
+++ _app/js/modules/projects.js
@@ -1,28 +1,30 @@
 export default function projects() {
-
 	const PROJECT_ID = "qe45c197";
 	const DATASET = "production";
 	const QUERY = encodeURIComponent('*[_type == "projects"]{url, title, _id, description, "imageUrl": image.asset->url}');
 	const URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`;
 
-
-	const project = document.querySelector('.project');
+	const projectsContainer = document.querySelector('.projects__container');
 
 	fetch(URL)
 		.then((res) => res.json())
 		.then(({ result }) => {
-			const projectImage = document.createElement('img');
-			const projectTitle = document.createElement('h4');
-			const projectDescription = document.createElement('p');
-			const projectUrl = document.createElement('a');
-			projectImage.setAttribute('src', result[0].imageUrl);
-			projectTitle.textContent = result[0].title;
-			projectDescription.textContent = result[0].description;
-			projectUrl.setAttribute('href', result[0].url);
-			projectUrl.textContent = 'See live project';
-			project.appendChild(projectImage);
-			project.appendChild(projectTitle);
-			project.appendChild(projectDescription);
-			project.appendChild(projectUrl);
+			for (const res of result) {
+				const project = document.createElement('div');
+				const projectImage = document.createElement('img');
+				const projectTitle = document.createElement('h4');
+				const projectDescription = document.createElement('p');
+				const projectUrl = document.createElement('a');
+				projectImage.setAttribute('src', res.imageUrl);
+				projectTitle.textContent = res.title;
+				projectDescription.textContent = res.description;
+				projectUrl.setAttribute('href', res.url);
+				projectUrl.textContent = 'See live project';
+				project.appendChild(projectImage);
+				project.appendChild(projectTitle);
+				project.appendChild(projectDescription);
+				project.appendChild(projectUrl);
+				projectsContainer.appendChild(project);
+			}
 		})
 }
